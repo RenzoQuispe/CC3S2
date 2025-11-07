83f0ec0968d9ac8d63fe60c1455ede454a2e0c9f
RenzoQuispe <renzo123cd@gmail.com>
2025-11-07 10:51:59 -0500

    agregar laboratorio para la actividad 15

---
+++ Actividad15-CC3S2/Laboratorio/Mediator/dependency.py
@@ -0,0 +1,6 @@
+#dependency.py
+class DependsOn:
+    def __init__(self, resource_type, resource_id, attributes=None):
+        self.resource_type = resource_type
+        self.resource_id = resource_id
+        self.attributes = attributes or {}
